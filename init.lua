e60cd304330d4060a67b627da8bbe0cba8a6d107
ddom <dovydasd11@gmail.com>
2024-03-10 21:49:27 +0200

    fix imports on save

--- init.lua
+++ init.lua
@@ -908,5 +908,24 @@ require('lazy').setup {
 
 require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/lua/snippets/' }
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*.go' },
+  callback = function()
+    local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+    params.context = { only = { 'source.organizeImports' } }
+
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, 3000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
